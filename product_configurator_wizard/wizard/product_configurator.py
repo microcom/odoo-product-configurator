33b76c2dbe5b153cef04e53c2d3c62515fcfe619
Stephane Le Cornec <stephane.lecornec@gmail.com>
2019-03-08 11:13:07 -0500

    product_configurator_wizard - fix create/add value on the fly

--- product_configurator_wizard/wizard/product_configurator.py
+++ product_configurator_wizard/wizard/product_configurator.py
@@ -608,7 +608,11 @@ class ProductConfigurator(models.TransientModel):
                 on_change="onchange_attribute_value(%s, context)" % field_name,
                 default_focus="1" if attr_line == attr_lines[0] else "0",
                 attrs=str(attrs),
-                context="{'show_attribute': False}",
+                context=str({
+                    'show_attribute': False,
+                    'product_tmpl_id': wiz.product_tmpl_id.id,
+                    'default_attribute_id': attribute_id
+                }),
                 options=str({
                     'no_create': not attr_line.attribute_id.create_on_the_fly,
                     'no_create_edit': not attr_line.attribute_id.create_on_the_fly,
